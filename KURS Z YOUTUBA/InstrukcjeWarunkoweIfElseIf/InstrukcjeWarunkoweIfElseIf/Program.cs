1c87541028a7a1d44f71a214d1f934806ecb7285
Wieczur <michal.wieczorek070992@gmail.com>
2023-08-21 11:59:37 +0200

    Lesson

---
+++ KURS Z YOUTUBA/InstrukcjeWarunkoweIfElseIf/InstrukcjeWarunkoweIfElseIf/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstrukcjeWarunkoweIfElseIf
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instrukcje If
+
+            int a = 4;
+            
+            if(a > 0 && a > 5)
+            {
+                Console.WriteLine("A jest wieksze od 0");
+                Console.WriteLine("A jest wieksze od 5");                
+            }
+            else if (a > 0)
+            {
+                Console.WriteLine("A jest wieksze od 0");
+            }
+            else if(a == 0)
+            {
+                Console.WriteLine("A jest rowne 0");
+            }
+            else
+            {
+                Console.WriteLine("A jest mniejsza od 0");      
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ KURS Z YOUTUBA/PetleWhileDoWhileFor/PetleWhileDoWhileFor/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetleWhileDoWhileFor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // PETLE | WHILE | FOR | DO WHILE
+
+            // Petla While(dopoki)
+
+            int a = 0;
+
+            // while (a < 5)
+            // {
+            //    Console.WriteLine(a);
+            //    Console.WriteLine("tekst");
+            //    a++;
+            // }
+
+            /* w petli for zadeklarowana zmienna b, jest przypisana tylko w petli, po za nia nie istnieje, nie mamy do niej dostepu, jezeli chcesz miec do niej dostep
+               zadeklaruj zmienna przed pettla for
+            */
+
+            // for (int b = 0 ; b < 5 ; b++) // kazda z czesci for jest opcjonalna moze byc a nie musi.
+            // {
+            //    Console.WriteLine(b);
+            //    Console.WriteLine("tekst");
+            // }
+
+            do // Petla Do ma zagwarantowane to ze petla wykona sie co najmniej raz, co najmniej raz wykona instrukcje zawarte w klamrach
+            {
+                Console.WriteLine("tekst");
+            }
+            while (false);
+            
+
+
+            Console.ReadKey();
+        }
+    }
+}
