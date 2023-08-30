2ad9787feeb52ba1143c83f68b6076119474d6af
Wieczur <michal.wieczorek070992@gmail.com>
2023-08-30 18:28:41 +0200

    Lesson

---
+++ KURS Z YOUTUBA/FunkcjeCz1/FunkcjeCz1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunkcjeCz1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Dodaj(2,4, d:2);
+            Console.ReadKey();
+        }
+
+        //static void Dodaj(int a, int b, int c = 0)
+        //{
+        //    int wynik = a + b + c;
+        //    Console.WriteLine("Wynik dodawania: " + wynik);
+        //}
+
+        //static void Dodaj(int a, int b, int c)
+        //{
+        //    int wynik = a + b + c;
+        //    Console.WriteLine("Wynik dodawania: " + wynik);
+        //}
+
+        static void Dodaj(int a, int b, int c = 2, int d = 0)
+        {
+            int wynik = a + b + c + d;
+            Console.WriteLine("Wynik dodawania: " + wynik);
+        }
+
+    }
+}
+
+// Fundament Programowania Obiektowego - nigdy nie powtarzaj tego co zrobiles wczesniej, nie marnuj czasu i nie pisz tego po raz kolejny !!!!!!!!!!!!!!!!!!!!!!!!! 
---
+++ KURS Z YOUTUBA/FunkcjeCz2/FunkcjeCz2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunkcjeCz2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Dodaj(2, 4));      
+            Console.ReadKey();
+        }
+
+        //static int Dodaj(int a, int b)
+        //{
+        //    int wynik = a + b;
+        //    return wynik;
+        //}
+       
+        //static int Dodaj(int a, int b)
+        //{
+        //    int wynik = a + b;
+        //    if(wynik >= 10)
+        //    {
+        //        return wynik;
+        //    }
+        //    else
+        //    {
+        //        return wynik;
+        //    }
+        //}
+
+        static string Dodaj(int a, int b)
+        {
+            int wynik = a + b;
+            if (wynik >= 10)
+            {
+                return "wynik wiekszy od 10";
+            }
+            else
+            {
+                return "wynik mniejszy od 10";
+            }
+        }
+    }
+}
+
