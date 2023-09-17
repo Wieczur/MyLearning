c74bca214fee38bee588f7c5f04efc971106f35c
Wieczur <michal.wieczorek070992@gmail.com>
2023-09-17 17:50:44 +0200

    Lesson

--- Witcher/Witcher/Program.cs
+++ Witcher/Witcher/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 using static System.Net.Mime.MediaTypeNames;
@@ -16,6 +17,11 @@ namespace Witcher
 
         static void Main(string[] args)
         {
+            ProgramStart();
+        }
+        static void ProgramStart()
+        {
+            
             // Hp Wiedzmina
             int healthPointWitcher = MaxWitcherHealth;
             // Obrazenia Wiedzmina
@@ -36,25 +42,15 @@ namespace Witcher
             int healthPointDrownAfterCombat;
             int healthPointWitcherAfterCombat;
             // Runda
-            int roundNumber = 1;
-            int afterRound = 1;
-            
-            
+            int roundNumber = 1;          
+            PrintInitialStats(healthPointWitcher, damageWitcher, cricitalChanceWitcher, healthPointDrown, damageDrown, cricitalChanceDrown);
 
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("Witcher = " + "healhPoint: " + healthPointWitcher + " , " + "Damage: " + damageWitcher + " , " + "CricitalStrike: " + cricitalChanceWitcher);
-            Console.ResetColor();
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("Drown = " + "healPoint: " + healthPointDrown + " , " + "Damage: " + damageDrown + " , " + "CricitalStrike: " + cricitalChanceDrown);
-            Console.ResetColor();
-            Console.ReadKey();
 
 
             while (healthPointWitcher > 0)
             {
-                Console.WriteLine("* * * * * * * * * * * *");
-                Console.WriteLine("Round: {0}", roundNumber);
-
+                PrintStatsBeforeRound(roundNumber);
+                
                 WitcherAction(healthPointWitcher, damageWitcher, healthPotion, cricitalStrikeDamageWticher, healthPointDrown, out healthPointDrownAfterCombat, out healthPointWitcherAfterCombat);
                 healthPointDrown = healthPointDrownAfterCombat;
                 healthPointWitcher = healthPointWitcherAfterCombat;
@@ -63,19 +59,10 @@ namespace Witcher
                 healthPointDrown = healthPointDrownAfterCombat;
                 healthPointWitcher = healthPointWitcherAfterCombat;
 
-                Console.ReadKey();
+                PrintStatsAfterRound(roundNumber, healthPointWitcher, healthPointDrown);
+
                 roundNumber++;
-
-                Console.WriteLine("After Round: {0}", afterRound);
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("Witcher = " + "healthPoint: " + healthPointWitcher);                             
-                Console.ResetColor();
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Drown = " + "healthPoint: " + healthPointDrown);
-                Console.ResetColor();  
-                Console.ReadKey();
-
-                afterRound++;
+      
             }
 
         }
@@ -91,42 +78,69 @@ namespace Witcher
         {
             healthPointDrownAfterCombat = healthPointDrown;
             healthPointWitcherAfterCombat = healthPointWitcher;
-
+          
             if (healthPointWitcher >= 30)
             {
-                Random rnd = new Random();
-                int randomNumber = (rnd.Next(1, 11));
-                int sampleNumber = 5;
-                if(randomNumber == sampleNumber)
-                {
-                    damageWitcher = cricitalStrikeDamageWticher;
-                    Console.WriteLine("CricitalStrike: " + damageWitcher);
-                }
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("WitcherAttack: " + damageWitcher);
-                healthPointDrownAfterCombat = healthPointDrown - damageWitcher;                
-                Console.ResetColor();
-  
+
+                damageWitcher = CountCricitalStrike(damageWitcher, cricitalStrikeDamageWticher);
+                healthPointDrownAfterCombat = DealDamageAsAWitcher(damageWitcher, healthPointDrownAfterCombat, healthPointDrown);
+       
             }
             else if (healthPointWitcher < 30)
             {
-                Console.ForegroundColor = ConsoleColor.Blue;
-                Console.WriteLine("UseHealPotion: " + healPotion);
-                
-                healthPointWitcherAfterCombat = healthPointWitcherAfterCombat + healPotion;
-                Console.WriteLine("Witcher Health After Heal: " + healthPointWitcherAfterCombat);
-                Console.ResetColor();
+                healthPointWitcherAfterCombat = WitcherHealMySelf(healPotion, healthPointWitcherAfterCombat);
             }
             else if (healthPointWitcher == 0)
             {
 
-                Console.ForegroundColor = ConsoleColor.DarkRed;
-                Console.WriteLine("WitcherDead");               
-                Console.ResetColor();
-                Console.ReadKey();
+                WitcherDead();
             }
+            
         }
-        
+        static void WitcherDead()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("WitcherDead");
+            Console.ResetColor();
+            Console.ReadKey();
+
+        }
+        static int WitcherHealMySelf(int healPotion, int healthPointWitcherAfterCombat)
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("UseHealPotion: " + healPotion);
+
+            healthPointWitcherAfterCombat = healthPointWitcherAfterCombat + healPotion;
+            Console.WriteLine("Witcher Health After Heal: " + healthPointWitcherAfterCombat);
+            Console.ResetColor();
+            return healthPointWitcherAfterCombat;
+        }
+
+        static int CountCricitalStrike(int damageWitcher, int cricitalStrikeDamageWticher)
+        {
+            Random rnd = new Random();
+            int randomNumber = (rnd.Next(1, 11));
+            int sampleNumber = 5;
+
+            if (randomNumber == sampleNumber)
+            {
+                damageWitcher = cricitalStrikeDamageWticher;
+                Console.WriteLine("CricitalStrike: " + damageWitcher);
+            }
+
+            return damageWitcher;
+        }
+        static int DealDamageAsAWitcher(int damageWitcher, int healthPointDrownAfterCombat, int healthPointDrown)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("WitcherAttack: " + damageWitcher);
+            healthPointDrownAfterCombat = healthPointDrown - damageWitcher;
+            Console.ResetColor();
+
+            return healthPointDrownAfterCombat;
+
+        }
+       
         static void DrownAction(
             int healthPointDrown, 
             int damageDrown, 
@@ -180,6 +194,34 @@ namespace Witcher
 
             }
    
+        }
+        
+        static void PrintInitialStats(int healthPointWitcher, int damageWitcher, int cricitalChanceWitcher, int healthPointDrown, int damageDrown, int cricitalChanceDrown)
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Witcher = " + "healhPoint: " + healthPointWitcher + " , " + "Damage: " + damageWitcher + " , " + "CricitalStrike: " + cricitalChanceWitcher);
+            Console.ResetColor();
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Drown = " + "healPoint: " + healthPointDrown + " , " + "Damage: " + damageDrown + " , " + "CricitalStrike: " + cricitalChanceDrown);
+            Console.ResetColor();
+            Console.ReadKey();
+        }
+        static void PrintStatsBeforeRound(int roundNumber)
+        {
+            Console.WriteLine("* * * * * * * * * * * *");
+            Console.WriteLine("Round: {0}", roundNumber);
+
+        }
+        static void PrintStatsAfterRound(int roundNumber, int healthPointWitcher, int healthPointDrown)
+        {
+                Console.WriteLine("After Round: {0}", roundNumber);
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Witcher = " + "healthPoint: " + healthPointWitcher);
+                Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Drown = " + "healthPoint: " + healthPointDrown);
+                Console.ResetColor();
+                Console.ReadKey();               
         }
         
     }
