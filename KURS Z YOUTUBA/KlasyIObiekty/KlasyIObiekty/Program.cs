f90bc30cf8bf573e00f82783fa78051050bf0eb5
Wieczur <michal.wieczorek070992@gmail.com>
2023-09-19 11:56:59 +0200

    Lesson

---
+++ KURS Z YOUTUBA/KlasyIObiekty/KlasyIObiekty/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KlasyIObiekty
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Czlowiek adi = new Czlowiek();
+            Console.WriteLine(adi.imie);
+            adi.imie = "Bartek";
+            Console.WriteLine(adi.imie);
+
+            Czlowiek adi2 = new Czlowiek();
+            Console.WriteLine(adi2.imie);
+
+            adi.PrzedstawSie();
+
+            Console.ReadKey();
+        }
+    }
+    //class Czlowiek
+    //{
+    //    public string imie = "Adrian";
+    //    public string nazwisko = "Kowalski";
+
+    //    public void PrzedstawSie()
+    //    {
+    //        Console.WriteLine("Mam na imie " + imie);
+    //    }
+    //}
+}
