8cb7efaa70e0f34f5aa08ee7a0e1aac1b5018cb9
Wieczur <michal.wieczorek070992@gmail.com>
2023-10-17 11:04:40 +0200

    Lesson

---
+++ KURS Z YOUTUBA/OPPDziedziczenie/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dziedziczenie
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Ziemia ziemia = new Ziemia();
+            ziemia.RuchObrotowy();
+            ziemia.PoraDniaINocy();
+            ziemia.Atmosfera();
+            
+            Console.ReadKey();
+        }
+    }
+
+    abstract class CialoNiebieskie
+    {
+        public string Nazwa { get; set; }
+        public void RuchObrotowy()
+        {
+            Console.WriteLine("Kazde Cialo niebieskie posiada ruch obrotowy");
+        }
+    }
+    abstract class Planeta : CialoNiebieskie
+    {
+        public void PoraDniaINocy()
+        {
+            Console.WriteLine("Pory Dnia i Nocy");
+        }
+    }
+    class Ziemia : Planeta
+    {
+        public void Atmosfera()
+        {
+            Console.WriteLine("Atmosfera Ziemska");
+        }
+
+    }
+    
+}
